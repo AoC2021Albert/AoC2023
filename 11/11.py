43f2a8ffef4701e258cbd8fe374af3f5ba44e05a
Albert Bendicho <abendicho@hopper.com>
2023-12-11 07:17:49 +0000

    day 11 review

--- 11/11.py
+++ 11/11.py
@@ -1,35 +1,29 @@
 #!/usr/bin/env python
-f = open("11/in.raw", "r")
-# f = open("11/sample.raw", "r")
-lines = f.read().splitlines()
 
-EXPANSE = 999999
-
-
-def star_distance(map, EXPANSE):
-    X_SIZE = len(map[0])
-    Y_SIZE = len(map)
-    offsetcols = [0] * X_SIZE
-    offsetrows = [0] * Y_SIZE
-    emptycols = [True] * X_SIZE
+def star_distances(map, EXPANSE):
+    col_offsets = []
+    row_offsets = []
+    empty_cols = [True] * len(map[0])
+    row_offset = 0
     # Scan de map row by row
     for y, row in enumerate(map):
+        row_offsets.append(row_offset)
         # If no stars in row
         if row.find('#') == -1:
             # Add EXPANSE offset to subsequent rows
-            for i in range(y + 1, Y_SIZE):
-                offsetrows[i] += EXPANSE
+            row_offset += EXPANSE
         # Detect Columns that have stars on this row
         for x, c in enumerate(row):
             if c == '#':
-                emptycols[x] = False
+                empty_cols[x] = False
 
+    col_offset = 0
     # For each column
-    for x, is_empty in enumerate(emptycols):
+    for x, is_empty in enumerate(empty_cols):
+        col_offsets.append(col_offset)
         if is_empty:
             # Add EXPANSE offset to subsecuent columns
-            for i in range(x + 1, X_SIZE):
-                offsetcols[i] += EXPANSE
+            col_offset += EXPANSE
 
     # With offsetcols and offsetrows ready
     # find real position of stars
@@ -37,19 +31,24 @@ def star_distance(map, EXPANSE):
     for y, row in enumerate(map):
         for x, c in enumerate(row):
             if row[x] == '#':
-                stars.append((y + offsetrows[y],
-                              x + offsetcols[x]))
+                stars.append((y + row_offsets[y],
+                              x + col_offsets[x]))
 
-    distance_sum = 0
+    ret = 0
     # The distance is the X-distance + Y-distance
     # For each star
     for i, star in enumerate(stars):
         # Check distance to subsequent stars
         for j in range(i + 1, len(stars)):
-            distance_sum += abs(star[0] - stars[j][0])
-            distance_sum += abs(star[1] - stars[j][1])
+            ret += abs(star[0] - stars[j][0])
+            ret += abs(star[1] - stars[j][1])
 
-    return (distance_sum)
+    return (ret)
 
-print(f'Part 1: {star_distance(lines,1)}')
-print(f'Part 2: {star_distance(lines,999999)}')
+
+f = open("11/in.raw", "r")
+# f = open("11/sample.raw", "r")
+lines = f.read().splitlines()
+
+print(f'Part 1: {star_distances(lines,1)}')
+print(f'Part 2: {star_distances(lines,999999)}')
