e286e174748d9ea6fb1d837515746b2cab283a9d
Albert Bendicho <abendicho@hopper.com>
2023-12-11 05:32:31 +0000

    day 11 part 2

--- 11/11.py
+++ 11/11.py
@@ -10,35 +10,39 @@ f = open("11/in.raw", "r")
 #f = open("11/sample.raw", "r")
 lines = f.read().splitlines()
 
+EXPANSE=999999
 res=0
-emptycol=[True]*len(lines[0])
+offsetcols=[0]*len(lines[0])
+offsetlines=[0]*len(lines)
+emptycols=[True]*len(lines[0])
 emptyline="."*len(lines[0])
-newlines=[]
-for line in lines:
+for i, line in enumerate(lines):
     if line==emptyline:
-        newlines.append(emptyline)
-    newlines.append(line)
+        for j in range(i+1,len(lines)):
+           offsetlines[j]+=EXPANSE
     for i, c in enumerate(line):
         if c == '#':
-            emptycol[i]=False
-
-lines = []
+            emptycols[i]=False
+for i, empty in enumerate(emptycols):
+    if empty:
+        for j in range(i+1,len(lines[0])):
+            offsetcols[j]+=EXPANSE
 stars = []
-for y, newline in enumerate(newlines):
-    line = ""
-    for i, c in enumerate(newline):
-        if emptycol[i]:
-            line+='.'
-        line+=c
-    lines.append(line)
+print(offsetlines,offsetcols)
+
+for y, line in enumerate(lines):
     for x,c in enumerate(line):
         if line[x]=='#':
-            stars.append((y,x))
+            print(f'start at {(y,x)} goes to {(y+offsetlines[y],x+offsetcols[x])}')
+            stars.append((y+offsetlines[y],x+offsetcols[x]))
 
 for i, star in enumerate(stars):
     for j in range(i+1,len(stars)):
+        pprint((star,stars[j]))
         res+=abs(star[0]-stars[j][0])
+        print(res)
         res+=abs(star[1]-stars[j][1])
+        print(res)
 
 
 print(res)
