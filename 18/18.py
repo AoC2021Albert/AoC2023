6d84dcf4db1778331ff26810eb0b49a92a928856
Albert Bendicho <abendicho@hopper.com>
2023-12-18 07:24:06 +0000

    Day 18 done

--- 18/18.py
+++ 18/18.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 f = open("18/in.raw", "r")
-#f = open("18/sample.raw", "r")
+# f = open("18/sample.raw", "r")
 lines = f.read().splitlines()
-U, R, D, L = 0, 1, 2, 3
-DIR = {
-    'R': R,
-    'D': D,
-    'L': L,
-    'U': U,
-}
+DIR = 'RDLU'
 next_step = [(1), (1j), (-1), (-1j)]
 
 
 def get_data1(line):
     direction, steps, _ = line.split(' ')
-    direction = DIR[direction]
+    direction = DIR.find(direction)
     steps = int(steps)
     return (direction, steps)
 
@@ -23,12 +17,12 @@ def get_data2(line):
     hex_digits = line.split(' ')[-1][2:-1]
     steps = int(hex_digits[:5], 16)
     direction = int(hex_digits[-1])
-    # Note: directions don't match
     return (direction, steps)
 
 
 def solve(lines, get_data):
     # Note: assuming clockwise
+    # https://en.wikipedia.org/wiki/Shoelace_formula for area
     area = 0.0
     pos = 0+0j
     direction, steps = get_data(lines[-1])  # Direction of last
@@ -37,11 +31,14 @@ def solve(lines, get_data):
         new_pos = pos+next_step[new_direction] * steps
         area += pos.real * new_pos.imag
         area -= pos.imag * new_pos.real
+        # shoelace area "extension" for corners
         if new_direction == (direction + 1) % 4:
             area += 3/2
         else:
             area += 1/2
+        # shoelace area "extension" for lines
         area += steps - 1
+
         pos = new_pos
         direction = new_direction
     new_pos = 0+0j
