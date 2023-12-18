4f2fbd9f95e6a48e86bdd10b673ca7abfcf533d0
Albert Bendicho <abendicho@hopper.com>
2023-12-18 07:16:27 +0000

    Day 18 unified

--- 18/18.py
+++ 18/18.py
@@ -1,53 +1,39 @@
 #!/usr/bin/env python
 f = open("18/in.raw", "r")
-f = open("18/sample.raw", "r")
+#f = open("18/sample.raw", "r")
 lines = f.read().splitlines()
 U, R, D, L = 0, 1, 2, 3
 DIR = {
-    'U': U,
     'R': R,
     'D': D,
     'L': L,
+    'U': U,
 }
-next_step = [(-1j), (1), (1j), (-1)]
+next_step = [(1), (1j), (-1), (-1j)]
 
 
-def solve1(lines):
+def get_data1(line):
+    direction, steps, _ = line.split(' ')
+    direction = DIR[direction]
+    steps = int(steps)
+    return (direction, steps)
+
+
+def get_data2(line):
+    hex_digits = line.split(' ')[-1][2:-1]
+    steps = int(hex_digits[:5], 16)
+    direction = int(hex_digits[-1])
+    # Note: directions don't match
+    return (direction, steps)
+
+
+def solve(lines, get_data):
     # Note: assuming clockwise
     area = 0.0
     pos = 0+0j
-    direction = DIR[lines[-1][0]]  # Direction of last
+    direction, steps = get_data(lines[-1])  # Direction of last
     for line in lines:
-        new_direction, steps, color = line.split(' ')
-        new_direction = DIR[new_direction]
-        steps = int(steps)
-        new_pos = pos + next_step[new_direction] * steps
-        area += pos.real * new_pos.imag
-        area -= pos.imag * new_pos.real
-        if new_direction == (direction + 1) % 4:
-            area += 3/2
-        else:
-            area += 1/2
-        area += steps - 1
-        pos = new_pos
-        direction = new_direction
-    new_pos = 0+0j
-    area += pos.real * new_pos.imag
-    area -= pos.imag * new_pos.real
-    area = area / 2
-    return (int(area))
-
-
-def solve2(lines):
-    # Note: assuming clockwise
-    area = 0.0
-    direction = int(lines[-1][-2])  # Direction of last
-    pos = 0+0j
-    for line in lines:
-        new_direction, steps, color = line.split(' ')
-        color = color[2:-1]
-        steps = int(color[:5], 16)
-        new_direction = int(color[-1])
+        new_direction, steps = get_data(line)
         new_pos = pos+next_step[new_direction] * steps
         area += pos.real * new_pos.imag
         area -= pos.imag * new_pos.real
@@ -65,5 +51,5 @@ def solve2(lines):
     return (int(area))
 
 
-print(solve1(lines))
-print(solve2(lines))
+print(solve(lines, get_data1))
+print(solve(lines, get_data2))
