b83dcc1d8143f6d37c9bed07091c9a9e84010b0a
Albert Bendicho <abendicho@hopper.com>
2023-12-18 06:22:50 +0000

    Day 18 clean

--- 18/18.py
+++ 18/18.py
@@ -1,80 +1,69 @@
 #!/usr/bin/env python
-from collections import defaultdict, deque
-from pprint import pprint
-from functools import reduce
-from operator import mul
-import re
-import math
-
 f = open("18/in.raw", "r")
-#f = open("18/sample.raw", "r")
+f = open("18/sample.raw", "r")
 lines = f.read().splitlines()
-pprint(lines)
-H = len(lines)
-L = len(lines[0])
-
-
 U, R, D, L = 0, 1, 2, 3
-DIR={
-    'U' : U,
-    'R' : R,
-    'D' : D,
-    'L' : L,    
+DIR = {
+    'U': U,
+    'R': R,
+    'D': D,
+    'L': L,
 }
 next_step = [(-1j), (1), (1j), (-1)]
 
+
 def solve1(lines):
     # Note: assuming clockwise
-    area=0.0
-    pos=0+0j
-    direction=U
+    area = 0.0
+    pos = 0+0j
+    direction = DIR[lines[-1][0]]  # Direction of last
     for line in lines:
         new_direction, steps, color = line.split(' ')
-        new_direction=DIR[new_direction]
-        steps=int(steps)
-        new_pos= pos+next_step[new_direction]*steps
-        area += pos.real*new_pos.imag
-        area -= pos.imag*new_pos.real
-        if new_direction == (direction + 1) %4:
-            area+=3/2
+        new_direction = DIR[new_direction]
+        steps = int(steps)
+        new_pos = pos + next_step[new_direction] * steps
+        area += pos.real * new_pos.imag
+        area -= pos.imag * new_pos.real
+        if new_direction == (direction + 1) % 4:
+            area += 3/2
         else:
-            area+=1/2
-        area+=steps-1
+            area += 1/2
+        area += steps - 1
         pos = new_pos
         direction = new_direction
     new_pos = 0+0j
-    area += pos.real*new_pos.imag
-    area -= pos.imag*new_pos.real
-    area = area/2
-    return(area)
+    area += pos.real * new_pos.imag
+    area -= pos.imag * new_pos.real
+    area = area / 2
+    return (int(area))
 
 
 def solve2(lines):
     # Note: assuming clockwise
-    pos=0+0j
-    direction=U
-    area=1.0
+    area = 0.0
+    direction = int(lines[-1][-2])  # Direction of last
+    pos = 0+0j
     for line in lines:
         new_direction, steps, color = line.split(' ')
-        color=color[2:-1]
-        steps=int(color[:5],16)
-        new_direction=int(color[-1])
-        new_pos= pos+next_step[new_direction]*steps
-        area += pos.real*new_pos.imag
-        area -= pos.imag*new_pos.real
-        if new_direction == (direction + 1) %4:
-            area+=3/2
+        color = color[2:-1]
+        steps = int(color[:5], 16)
+        new_direction = int(color[-1])
+        new_pos = pos+next_step[new_direction] * steps
+        area += pos.real * new_pos.imag
+        area -= pos.imag * new_pos.real
+        if new_direction == (direction + 1) % 4:
+            area += 3/2
         else:
-            area+=1/2
-        area+=steps-1
+            area += 1/2
+        area += steps - 1
         pos = new_pos
         direction = new_direction
     new_pos = 0+0j
-    area += pos.real*new_pos.imag
-    area -= pos.imag*new_pos.real
-    area = area/2
-    return(area)
+    area += pos.real * new_pos.imag
+    area -= pos.imag * new_pos.real
+    area = area / 2
+    return (int(area))
+
 
 print(solve1(lines))
-
 print(solve2(lines))
