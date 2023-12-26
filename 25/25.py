48fe55133683074659b3495f0083600aafa5f8fb
Albert Bendicho <abendicho@hopper.com>
2023-12-26 00:08:05 +0000

    Day 25 completed

--- 25/25.py
+++ 25/25.py
@@ -10,107 +10,95 @@ import sys
 sys.setrecursionlimit(100000)
 
 f = open("25/in.raw", "r")
-#f = open("25/sample.raw", "r")
+# f = open("25/sample.raw", "r")
 lines = f.read().splitlines()
 
 res = 0
-edges = set()
 destinations = defaultdict(list)
 for i, line in enumerate(lines):
-    left,right = line.split(': ')
-    right=right.split(' ')
+    left, right = line.split(': ')
+    right = right.split(' ')
     for r in right:
-        edges.add(tuple(sorted([left, r])))
         destinations[left].append(r)
         destinations[r].append(left)
+# to_sample can be ANY node, we just need one, we get the last 'left'
+sample_node = left
 
-'''
-# remove all the edges that are loops
-def remove_loops(unlooped, path, element):
-    if element in path:
-        loop = path[path.index(element):len(path)]+[element]
-        for i in range(len(loop)-1):
-            edge = (loop[i],loop[i+1]) if loop[i]>loop[i+1] else (loop[i+1],loop[i])
-            if edge in unlooped:
-                unlooped.remove(edge)
-    else:
-        for d in destinations[element]:
-            edge=(element,d) if element>d else (d,element)
-            if edge in unlooped and d!=path[-1]:
-                remove_loops(unlooped, path+[element], d)
+# edge_visits dictionary
+#    Key   = edge (identified by a tuple of both nodes, sorted min,max )
+#    Value = number of visits the edge has on all min-paths from any node to any node
+# The idea is that most visited edges have better chances of being one of the three
+# that "connect" both sub-graphs.
+edge_visits = defaultdict(int)
+# We count min_paths from each starting node to each destination node
+# This will count things twice, as we record both a->b->c and c->b->a
+# although it might use different paths in one direction and the other.
+# The path on the return of the given example might be c->d->a
+# But this is not relevant to us. We just want edges with most passes
+# to optimize "brute force" search
+for src, dsts in destinations.items():
+    visited = set([src])
+    paths = deque()
+    for dst in dsts:
+        # The elements of the path are optimized for computin use of edges
+        # So a path will be ((edge,node,),(edge,node,),(edge,node,),)
+        # example, the path str->mid-lst will be represented as
+        # (('mid','str','mid'),('lst','mid','lst',))
+        # The first two values represent the edge, the last value is the
+        # last node in the path.
+        paths.append(((min(src, dst), max(src, dst), dst,),))
+        visited.add(dst)
+    while len(paths) > 0:
+        path = paths.popleft()
+        lastnode = path[-1][-1]
+        for dstnode in destinations[lastnode]:
+            if dstnode not in visited:
+                visited.add(dstnode)
+                newpath = path + \
+                    ((min(lastnode, dstnode), max(lastnode, dstnode), dstnode,),)
+                for edge in newpath:
+                    edge_visits[(edge[0], edge[1],)] += 1
+                paths.append(newpath)
 
-unlooped = deepcopy(edges)
-for edge in edges:
-    if edge in unlooped:
-        remove_loops(unlooped,[edge[0]],edge[1])
 
-# First attempts. Does not work. Would have only worked if there was a single connection between the two sub-graphs
-#pprint(unlooped)
+def remove(destinations, to_remove):
+    for src, dst in to_remove:
+        destinations[src].remove(dst)
+        destinations[dst].remove(src)
 
 
-# Going to try with counting how many times an edge participates in the shortest path between nodes.
-'''
-# first we get the mininum distances, mindists
-paths=[list(edge) for edge in edges]
-mindists={edge:list(edge) for edge in edges}
-while len(paths)>0:
-    next_paths=list()
-    for path in paths:
-        src, mid = path[0],path[-1]
-        for dst in destinations[mid]:
-            if dst not in path:
-                srcdst = tuple(sorted([src,dst]))
-                if srcdst not in mindists:
-                    mindists[srcdst]=path+[dst]
-                    next_paths.append(path+[dst])
-    paths = next_paths
+def readd(destinations, to_remove):
+    for src, dst in to_remove:
+        destinations[src].append(dst)
+        destinations[dst].append(src)
 
-#pprint(mindists)
 
-uses=defaultdict(int)
-for mindistpath in mindists.values():
-    for node in range(len(mindistpath)-1):
-        one, two = mindistpath[node], mindistpath[node+1]
-        edge = (one,two) if one>two else (two,one)
-        uses[edge]+=1
+def count_set(destintions, sample_node):
+    start = sample_node
+    visited = set([start])
+    candidates = set(destinations[start])
+    while candidates:
+        new_candidates = set()
+        for candidate in candidates:
+            if candidate not in visited:
+                visited.add(candidate)
+                new_candidates.update(destinations[candidate])
+        candidates = new_candidates
+    return (len(visited))
 
-populated=[]
-for k,v in uses.items():
-    populated.append((v,k))
 
-removal_candidates = sorted(populated,reverse=True)
-#pprint(removal_candidates)
-for a in range(2,len(removal_candidates)):
-#    print(a)
-    srca, dsta = sorted(removal_candidates[a][1])
-    destinations[srca].remove(dsta)
-    destinations[dsta].remove(srca)
-    for b in range(1,a):
-        srcb, dstb = sorted(removal_candidates[b][1])
-        destinations[srcb].remove(dstb)
-        destinations[dstb].remove(srcb)
-        for c in range(0,b):
-            srcc, dstc = sorted(removal_candidates[c][1])
-            destinations[srcc].remove(dstc)
-            destinations[dstc].remove(srcc)
-            start = dstc
-            visited=set([start])
-            candidates = set(destinations[start])
-            while candidates:
-                new_candidates = set()
-                for candidate in candidates:
-                    if candidate not in visited:
-                        visited.add(candidate)
-                        new_candidates.update(destinations[candidate])
-                candidates=new_candidates
-            if len(visited)!=len(destinations):
-                print((len(destinations)-len(visited)) * len(visited))
+removal_candidates = sorted(edge_visits.items(), key=lambda x: -x[1])
+to_remove = []
+for a in range(2, len(removal_candidates)):
+    to_remove.append(removal_candidates[a][0])
+    for b in range(1, a):
+        to_remove.append(removal_candidates[b][0])
+        for c in range(0, b):
+            to_remove.append(removal_candidates[c][0])
+            remove(destinations, to_remove)
+            set_size = count_set(destinations, sample_node)
+            if set_size != len(destinations):
+                print((len(destinations)-set_size) * set_size)
                 exit()
-            destinations[srcc].append(dstc)
-            destinations[dstc].append(srcc)
-        destinations[srcb].append(dstb)
-        destinations[dstb].append(srcb)
-    destinations[srca].append(dsta)
-    destinations[dsta].append(srca)
-
+            readd(destinations, to_remove)
 print('FAIL')
